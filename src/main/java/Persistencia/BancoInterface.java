053bf65148b99fb476c61bc42e930f14fcb4dd9d
Matheus Alves <matheuseejam@gmail.com>
2018-11-20 21:32:08 -0300

    Commit inicial

---
+++ src/main/java/Persistencia/BancoInterface.java
@@ -0,0 +1,10 @@
+package Persistencia;
+
+/*
+ * Esta interface define a forma como o sistema interage com o banco de dados.
+ * Abstrai a persistencia de dados no banco
+ * */
+
+public interface BancoInterface {
+
+}
